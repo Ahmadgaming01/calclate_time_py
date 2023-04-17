e8e64fab2eef00e68a74da7220c67cb825d188fd
ahmad <aalmadani111@gmail.com>
2023-04-17 23:53:29 +0200

    calculatetime task

---
+++ calculate_time_func.py
@@ -0,0 +1,21 @@
+import time
+import math
+def calculate_time(func):
+    def inner (*args,**kwargs):
+        start = time.time()
+        func(*args,**kwargs)
+        end = time.time()
+        print(f'Execution Time : {end-start}')
+        
+@calculate_time
+def myloop(n):
+    for x in range(n):
+        pass
+myloop(1000)
+
+@calculate_time
+
+def myloop2(s):
+    math.factorial(s)
+
+myloop(2000)
